7a237bf2e790fed25ed64d5b17ddcb0cc95bd09c
xenomech <gokulsmenon227@gmail.com>
2023-12-02 22:49:36 +0530

    feat: add tooltip to buttons

--- src/components/Letter.vue
+++ src/components/Letter.vue
@@ -2,42 +2,33 @@
   <section class="letter" ref="root">
     <section class="sidebar col-6@lg col-12">
       <div class="toolbar grid">
-        <button @click="animate" class="button col-2">
+        <button @click="animate" class="button col-2 tooltip">
+          <span class="tooltip-text">Animate</span>
           <span class="material-symbols-outlined">stylus_note</span>
         </button>
-
-        <button @click="dots" class="button col-2">
+        <button @click="dots" class="button col-2 tooltip">
+          <span class="tooltip-text">Switch to dots</span>
           <span class="material-symbols-outlined">page_control</span>
         </button>
-        <button @click="playSound" class="button col-2">
+        <button @click="playSound" class="button col-2 tooltip">
+          <span class="tooltip-text">Pronounce</span>
           <span class="material-symbols-outlined">record_voice_over</span>
         </button>
-        <button @click="clearCanvas" class="button col-2">
+        <button @click="clearCanvas" class="button col-2 tooltip">
+          <span class="tooltip-text">Clear canvas</span>
           <span class="material-symbols-outlined">clear</span>
         </button>
       </div>
     </section>
     <section class="letter-container col-12 grid">
       <letter-nav v-if="viewmode === 'letternav'" />
-      <div
-        class="letter-svg-background"
-        v-html="letterSVG"
-        v-if="viewmode === 'draw'"
-      />
-      <div
-        :class="`letter-svg ${viewmode}`"
-        v-html="letterSVG"
-        v-if="viewmode === 'draw' || viewmode === 'dots'"
-      />
+      <div class="letter-svg-background" v-html="letterSVG" v-if="viewmode === 'draw'" />
+      <div :class="`letter-svg ${viewmode}`" v-html="letterSVG" v-if="viewmode === 'draw' || viewmode === 'dots'" />
       <drawing-canvas ref="canvas" />
     </section>
     <div class="examples col-12 grid" v-if="examples?.length">
       <span class="example col-1@lg col-3"> Examples: </span>
-      <span
-        class="example col-1@lg col-3"
-        v-for="example in examples"
-        :key="example"
-      >
+      <span class="example col-1@lg col-3" v-for="example in examples" :key="example">
         {{ example }}
       </span>
     </div>
@@ -154,15 +145,18 @@ section.letter {
       stroke-opacity: 1 !important;
     }
   }
+
   .letter-svg {
     path {
       stroke-width: 18px !important;
       stroke: var(--blue-1) !important;
     }
+
     text {
       display: none;
     }
   }
+
   .letter-svg-background,
   .letter-svg {
     position: absolute;
@@ -170,12 +164,14 @@ section.letter {
     left: 0;
     height: 100% !important;
     width: 100%;
+
     svg {
       height: 100% !important;
       width: 100%;
     }
   }
 }
+
 audio {
   width: 100%;
 }
@@ -183,12 +179,22 @@ audio {
 .toolbar {
   grid-area: toolbar;
 }
+
 .toolbar button {
   margin: var(--size-1);
   background-color: var(--brand);
   color: var(--text-1);
   padding: var(--size-2);
   cursor: pointer;
+  transition: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+}
+
+.toolbar button:hover {
+  background-color: rgba(#228be6, 0.75);
+  color: var(--text-1);
+
 }
 
 .examples {
@@ -210,5 +216,46 @@ svg {
   width: 100%;
   height: 100%;
   position: absolute;
+}
+
+/************************
+*                       *  
+*     TOOLTIP CSS       *
+*                       *
+************************/
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltip-text {
+  visibility: hidden;
+  width: 120px;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  position: absolute;
+  z-index: 1;
+  width: 120px;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -60px;
+}
+
+.tooltip .tooltip-text::after {
+  content: " ";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: black transparent transparent transparent;
+}
+
+.tooltip:hover .tooltip-text {
+  visibility: visible;
 }
 </style>
